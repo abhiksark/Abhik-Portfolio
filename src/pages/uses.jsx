2b7d23ba9d379cc4b6fc276b9af2cd32ecda9808
Abhik <abhiksark@gmail.com>
2024-02-02 12:56:09 +0530

    Update tools section in uses.jsx

--- src/pages/uses.jsx
+++ src/pages/uses.jsx
@@ -3,18 +3,36 @@ import  siteMeta from '@/data/siteMeta'
 import { Card } from '@/components/Card'
 import { Section } from '@/components/Section'
 import { SimpleLayout } from '@/components/SimpleLayout'
+import React from 'react';
+import Link from 'next/link';
 
 function ToolsSection({ children, ...props }) {
   return (
     <Section {...props}>
-      <ul role="list" className="space-y-3">
+      <ul role="list" className="space-y-6">
         {children}
       </ul>
     </Section>
   )
 }
 
+
+
 function Tool({ title, href, children }) {
+  return (
+    <Card as="li">
+      <Card.Title as="h3">
+        {/* Wrapping the title in an <a> tag to make it clickable */}
+        <a href={href} className="text-zinc-800 hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500">{title}</a>
+      </Card.Title>
+      <Card.Description>{children}</Card.Description>
+    </Card>
+  )
+}
+
+
+
+function ToolOld({ title, href, children }) {
   return (
     <Card as="li">
       <Card.Title as="h3" href={href}>
@@ -51,18 +69,18 @@ export default function Uses() {
         title="Software I use, gadgets I love, and other things I recommend."
         intro="I get asked a lot about the things I use to build software, stay productive, or buy to fool myself into thinking I’m being productive when I’m really just procrastinating. Here’s a big list of all of my favorite stuff."
       >
-        <div className="space-y-10">
+        <div className="space-y-16">
           <ToolsSection title="Workstation">
-            <Tool title="13” MacBook M1 Air, 16GB RAM (2021)">
+            <Tool title="13” MacBook M1 Air, 16GB RAM (2021)" href="https://www.apple.com/macbook-air-m1/">
               RISC FTW. I love this machine. It&apos;s fast, light, and has great battery life.
             </Tool>
-            <Tool title="AMD Ryzen 9 + RTX A6000">
+            <Tool title="AMD Ryzen 9 + RTX A6000" href="https://www.nvidia.com/en-gb/design-visualization/rtx-a6000/">
               You need to train those models, right?
             </Tool>
-            <Tool title="12.9 inch iPad Pro">
+            <Tool title="12.9 inch iPad Pro" href="https://www.apple.com/in/ipad-pro/">
               I use this for reading, taking notes and my second screen while i am travelling.
             </Tool>
-            <Tool title="MSI GS66 Stealth">
+            <Tool title="MSI GS66 Stealth" href="https://www.msi.com/Laptop/GS66-Stealth-10SX">
               My Linux machine. I use this as my daily driver.
             </Tool>
 
@@ -71,25 +89,25 @@ export default function Uses() {
   
       
         <ToolsSection title="Software Section">
-          <Tool title="VS Code">
+          <Tool title="VS Code" href="https://code.visualstudio.com/">
             My favorite editor. I use it for everything.
           </Tool>
-          <Tool title="Barrier">
+          <Tool title="Barrier"  href="https://github.com/debauchee/barrier">
             Lifesafer for my workflow. I use it to share my mouse and keyboard between my Mac and Linux machine.
           </Tool>
-          <Tool title="Copilot">
+          <Tool title="Copilot" href="https://copilot.github.com/">
            Do I need to say anything?
           </Tool>
           
-          <Tool title="Obsidian">
+          <Tool title="Obsidian" href="https://obsidian.md/">
             My second brain. I use it for everything.
           </Tool>
 
-          <Tool title="Notion">
+          <Tool title="Notion" href="https://www.notion.so/">
             Mainly work related stuff.
           </Tool>
 
-          <Tool title="Byobu">
+          <Tool title="Byobu" href="https://byobu.org/">
             Gods gift to terminal multiplexing.
           </Tool>
 
