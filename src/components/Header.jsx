5be43baf290507ef4991e8bf3a84ae7993edd08a
Abhik <abhiksark@gmail.com>
2025-01-22 00:11:20 +0530

    feat: add 'Papers' link to mobile navigation in Header component
    - Introduced a new navigation item for 'Papers' in the mobile header, enhancing user access to the recently added papers on deep learning and transformer optimization.
    - This update improves the overall navigation experience and ensures that users can easily find relevant research content.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -68,6 +68,7 @@ function MobileNavigation(props) {
               <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
                 <MobileNavItem href="/about">About</MobileNavItem>
                 <MobileNavItem href="/articles">Articles</MobileNavItem>
+                <MobileNavItem href="/papers">Papers</MobileNavItem>
                 <MobileNavItem href="/resume">Resume</MobileNavItem>
                 <MobileNavItem href="/speaking">Speaking</MobileNavItem>
                 <MobileNavItem href="/uses">Uses</MobileNavItem>
